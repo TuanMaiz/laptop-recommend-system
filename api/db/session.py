e708d6d0b0b4501f6d9d0f52b4bf034f10395feb
KhoaLang <tkhoa882@gmail.com>
2025-08-16 18:17:22 +0700

    refine API

--- api/db/session.py
+++ api/db/session.py
@@ -1,10 +1,13 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:yourpassword@localhost:5432/mydatabase"
+load_dotenv()
+conn_string = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(conn_string)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
---
+++ api/routers/laptop_router.py
@@ -0,0 +1,36 @@
+import os
+from typing import List, Optional
+from fastapi import APIRouter, HTTPException
+from fastapi.params import Depends
+import psycopg2
+from sqlalchemy.orm import Session
+from api.db.session import SessionLocal
+from api.schemas.laptop_schema import LaptopBase
+from api.db.models.laptop_model import Laptop
+
+router = APIRouter(prefix="/laptops")
+
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.get("/", response_model=List[LaptopBase])
+def list_laptops(limit: int = 20, db: Session = Depends(get_db)):
+    laptops = db.query(Laptop).limit(limit).all()
+    if not laptops:
+        raise HTTPException(status_code=404, detail="No laptops found")
+    return laptops
+
+
+@router.get("/{id}", response_model=LaptopBase)
+def get_laptop_detail(id: int, db: Session = Depends(get_db)):
+    laptop = db.query(Laptop).filter(Laptop.id == id).first()
+    if not laptop:
+        raise HTTPException(status_code=404, detail="Laptop not found")
+    return laptop
